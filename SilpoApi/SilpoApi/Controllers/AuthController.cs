6072db173f3636d2fd842418c380c2f261afebbf
Oleksandr Panchuk <alekssen2008@gmail.com>
2026-04-09 20:15:58 +0300

    added register with MediatR

--- SilpoApi/SilpoApi/Controllers/AuthController.cs
+++ SilpoApi/SilpoApi/Controllers/AuthController.cs
@@ -1,4 +1,5 @@
 ﻿using Application.Auth.Login;
+using Application.Auth.Register;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,7 +8,7 @@ namespace SilpoApi.Controllers;
 
 [Route("api/[controller]/[action]")]
 [ApiController]
-public class AuthController(IMediator mediator) : ControllerBase
+public class AuthController(IMediator mediator) : Controller
 {
     [HttpPost]
     public async Task<IActionResult> Login([FromBody] LoginRequestDto request)
@@ -25,6 +26,21 @@ public class AuthController(IMediator mediator) : ControllerBase
 		catch (Exception ex)
 		{
 			return BadRequest(ex.Message); // повертаємо код 400 + помилку, якщо щось пішло не так
+        }
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Register([FromBody] RegisterCommand request)
+    {
+        try
+        {
+            // відправляємо команду до MediatR, яка обробить логіку аутентифікації
+            await mediator.Send(request);
+            return Ok(); // повертаємо результат у вигляді JSON
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message); // повертаємо код 400 + помилку, якщо щось пішло не так
         }
     }
 }
